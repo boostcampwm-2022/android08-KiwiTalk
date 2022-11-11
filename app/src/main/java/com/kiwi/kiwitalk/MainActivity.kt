002fbfb5bd8599282d1b781cdb9a364a2dce8d36
SteelZoo <cjfwn96@gmail.com>
2022-11-11 11:41:48 +0900

    init: 프로젝트 생성

---
+++ app/src/main/java/com/kiwi/kiwitalk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kiwi.kiwitalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
