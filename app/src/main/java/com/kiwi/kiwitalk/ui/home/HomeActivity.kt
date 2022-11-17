da7adbe8cf6262e05f85e08c0da6b6ffd429dcbb
ChoiUS <chldn323@gmail.com>
2022-11-17 22:37:11 +0900

    feat: HomeActivity 실행

--- app/src/main/java/com/kiwi/kiwitalk/ui/home/HomeActivity.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/home/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.kiwi.kiwitalk.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kiwi.kiwitalk.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/kiwi/kiwitalk/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.kiwi.kiwitalk.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kiwi.kiwitalk.R
+import com.kiwi.kiwitalk.ui.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        startActivity(Intent(applicationContext, HomeActivity::class.java))
     }
 }
