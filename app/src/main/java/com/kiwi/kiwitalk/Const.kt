cd3b0669629ed47ffa2bed843eabd9cd478c4870
KimGyeongyeon <kgy556@naver.com>
2022-11-17 13:14:38 +0900

    feat: 상태 체크 없이 splash 띄움
    - 160dp의 원에 다 들어가야하는데 그렇게 줄이면 사진화질이 박살남
    - API 31 이하에 대한 대응은 공식문서에서 쏙 빠졌음
    - 이제 콜백으로 사전 체크 끝나는 형식으로 바꿀 예정

---
+++ app/src/main/java/com/kiwi/kiwitalk/Const.kt
@@ -0,0 +1,5 @@
+package com.kiwi.kiwitalk
+
+object Const {
+    const val SPLASH_DURATION = 5000L
+}
--- app/src/main/java/com/kiwi/kiwitalk/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/login/LoginActivity.kt
@@ -1,12 +1,38 @@
 package com.kiwi.kiwitalk.ui.login
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import com.kiwi.kiwitalk.Const
 import com.kiwi.kiwitalk.R
 
 class LoginActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = Const.SPLASH_DURATION
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            // Run your animation.
+            slideUp.start()
+        }
     }
 }
