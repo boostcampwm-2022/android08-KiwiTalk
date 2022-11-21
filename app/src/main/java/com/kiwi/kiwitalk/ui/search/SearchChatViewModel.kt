580d35fd4c59ec32e943d991b5cc30f39695e747
ChoiUS <chldn323@gmail.com>
2022-11-21 23:41:14 +0900

    fix: SearchChatViewModel에 적용된 hilt 수정

--- app/src/main/java/com/kiwi/kiwitalk/ui/search/SearchChatViewModel.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/search/SearchChatViewModel.kt
@@ -2,10 +2,10 @@ package com.kiwi.kiwitalk.ui.search
 
 import androidx.lifecycle.ViewModel
 import com.kiwi.domain.repository.SearchChatRepository
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@ViewModelScoped
+@HiltViewModel
 class SearchChatViewModel @Inject constructor(
     private val searchChatRepository: SearchChatRepository
 ) : ViewModel() {
