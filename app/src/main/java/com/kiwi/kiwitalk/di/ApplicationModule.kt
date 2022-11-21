f8d114752949da5281ed82679bda533974846eee
KimGyeongyeon <kgy556@naver.com>
2022-11-21 22:23:37 +0900

    feature: 구글 로그인 서비스객체 di 추가
    - 로그인 서비스 객체를 생성자 통해서 주입받을 예정

--- app/src/main/java/com/kiwi/kiwitalk/di/ApplicationModule.kt
+++ app/src/main/java/com/kiwi/kiwitalk/di/ApplicationModule.kt
@@ -1,8 +1,13 @@
 package com.kiwi.kiwitalk.di
 
 import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.kiwi.kiwitalk.AppPreference
+import com.kiwi.kiwitalk.BuildConfig
 import com.kiwi.kiwitalk.Const
+import com.kiwi.kiwitalk.ui.login.LoginActivity
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,10 +17,19 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class ApplicationModule {
+object ApplicationModule {
     @Singleton
     @Provides
     fun providePreference(@ApplicationContext context: Context): AppPreference
-        = AppPreference(context.getSharedPreferences(Const.LOGIN_HISTORY_KEY, Context.MODE_PRIVATE))
+            = AppPreference(context.getSharedPreferences(Const.LOGIN_HISTORY_KEY, Context.MODE_PRIVATE))
 
+    @Singleton
+    @Provides
+    fun provideGoogleApiClient(@ApplicationContext context: Context): GoogleSignInClient {
+        val googleSignOptions: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.SERVER_CLIENT_ID)
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(context, googleSignOptions)
+    }
 }
