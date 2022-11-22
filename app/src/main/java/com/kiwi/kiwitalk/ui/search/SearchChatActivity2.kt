49ee27e59114d18eef70e6a2c2271c8d3e0fa64c
KimGyeongyeon <kgy556@naver.com>
2022-11-22 13:14:34 +0900

    feature: Floating button 추가
    - 새 톡방 만드는 버튼을 추가했음

---
+++ app/src/main/java/com/kiwi/kiwitalk/ui/search/SearchChatActivity2.kt
@@ -0,0 +1,12 @@
+package com.kiwi.kiwitalk.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kiwi.kiwitalk.R
+
+class SearchChatActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_chat_2)
+    }
+}
