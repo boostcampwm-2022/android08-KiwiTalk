a78bf8821065edb97112d3674df8e9c78a8dfef7
SteelZoo <cjfwn96@gmail.com>
2022-11-23 02:02:58 +0900

    feat: SearchKeywordViewModel/ firestore 에서 키워드 컬렉션 호출 확인

--- app/src/main/java/com/kiwi/kiwitalk/ui/keyword/SearchKeywordViewModel.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/keyword/SearchKeywordViewModel.kt
@@ -1,24 +1,33 @@
 package com.kiwi.kiwitalk.ui.keyword
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kiwi.domain.repository.SearchKeywordRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class SearchKeywordViewModel @Inject constructor(
-    val searchKeywordRepository: SearchKeywordRepository
+    private val searchKeywordRepository: SearchKeywordRepository
 ): ViewModel() {
+
     private val _keywords = MutableLiveData<String>()
     val keywords: LiveData<String>
         get() = _keywords
 
-    fun getKeywords(){
+
+    fun getAllKeywords(){
         viewModelScope.launch {
-            _keywords.value = searchKeywordRepository.getAllKeyWord()
+            searchKeywordRepository.getAllKeyWord().also { Log.d("FIRESTORE_CALL_KEYWORD", "getKeywords: VM 1 ${it}") }
+                ?.let {
+                    _keywords.value = it
+                }
         }
+        Log.d("FIRESTORE_CALL_KEYWORD", "getKeywords: VM 2")
     }
 
 }
--- data/src/main/java/com/kiwi/data/datasource/remote/SearchKeywordRemoteDataSource.kt
+++ data/src/main/java/com/kiwi/data/datasource/remote/SearchKeywordRemoteDataSource.kt
@@ -5,27 +5,32 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class SearchKeywordRemoteDataSource @Inject constructor(
     val firestore: FirebaseFirestore
 ) {
 
-    suspend fun callAllKeyword(): String?{
-        var resultString: String? = null
-        val firestoreAsync = CoroutineScope(Dispatchers.Default).async {
-            firestore.collection("keywords").get()
+    suspend fun callAllKeyword(): String? {
+        var resultString: String = ""
+
+
+        firestore.collection("keywords").get()
                 .addOnSuccessListener { result ->
                     result.documents.forEach {
-                        resultString += it.data?.toString()
+                        resultString += it.id
+                        Log.d("FIRESTORE_CALL_KEYWORD", "callAllKeyword: sucess data : ${it.id}")
                     }
+                    Log.d("FIRESTORE_CALL_KEYWORD", "callAllKeyword: sucess resultString : ${resultString}")
                 }
                 .addOnFailureListener {
-                    Log.d("FIRESTORE_CALL_KEYWORD", "callAllKeyword: ")
-                }
-        }
-        firestoreAsync.await()
-        return resultString
+                    Log.d("FIRESTORE_CALL_KEYWORD", "callAllKeyword: fail")
+                }.await()
+
+
+        return resultString.also { Log.d("FIRESTORE_CALL_KEYWORD", "return: ") }
     }
 
 }
