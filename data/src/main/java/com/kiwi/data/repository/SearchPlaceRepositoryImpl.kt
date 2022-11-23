b73fc0b9a5d9d7d8aee02e436e9e898f2b742778
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2022-11-23 11:32:20 +0900

    feat: 장소 정보를 가져올 repository 구현

---
+++ data/src/main/java/com/kiwi/data/repository/SearchPlaceRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.kiwi.data.repository
+
+import com.kiwi.data.datasource.remote.SearchPlaceRemoteDataSource
+import com.kiwi.domain.ApiResult
+import com.kiwi.domain.ResultSearchPlace
+import com.kiwi.domain.repository.SearchPlaceRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchPlaceRepositoryImpl @Inject constructor(
+    private val dataSource: SearchPlaceRemoteDataSource
+): SearchPlaceRepository {
+
+    override suspend fun getSearchKeyword(
+        lng: String,
+        lat: String,
+        place: String
+    ): ApiResult<ResultSearchPlace> {
+        return dataSource.getSearchKeyword(lng,lat,place)
+    }
+}
