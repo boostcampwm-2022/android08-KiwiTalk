75bc031e16a2ddc75b30d75369a864f3100ad0d1
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2022-11-27 22:40:35 +0900

    feat: 채팅방 생성 클릭 시 ViewModel 호출 & EditText Null Check

--- app/src/main/java/com/kiwi/kiwitalk/ui/newchat/NewChatFragment.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/newchat/NewChatFragment.kt
@@ -54,16 +54,66 @@ class NewChatFragment : Fragment() {
             binding.tvChatSelectAddress.text = address
         }
 
-        binding.btnNewChat.setOnClickListener {
-            val intent = Intent(requireActivity(),HomeActivity::class.java)
-            startActivity(intent)
+        newChatViewModel.isChatImage.observe(viewLifecycleOwner) {
+            setImage(binding.ivChatImage, it)
+        }
+
+        newChatViewModel.isNewChatInfo.observe(viewLifecycleOwner) {
+            newChatViewModel.addNewChat(it)
+             val intent = Intent(requireActivity(), HomeActivity::class.java)
+              startActivity(intent)
         }
 
         binding.btcChatAddress.setOnClickListener {
             findNavController().navigate(R.id.action_newChatFragment_to_searchPlaceFragment)
         }
+        binding.btnNewChat.setOnClickListener {
+            if (allCheckNull()) {
+                newChatViewModel.setNewChat(changeChatInfo())
+            }
+        }
     }
-    
+
+    private fun changeChatInfo(): NewChat {
+        with(newChatViewModel) {
+            return NewChat(
+                isChatImage.value,
+                binding.etChatName.text.toString(),
+                binding.etChatDescription.text.toString(),
+                binding.etChatMaxPersonnel.text.toString(),
+                listOf("운동", "카페"),
+                isAddress.value ?: "",
+                isLatLng.value?.latitude ?: 0.0,
+                isLatLng.value?.longitude ?: 0.0
+            )
+        }
+    }
+
+    private fun allCheckNull(): Boolean {
+        with(binding) {
+            if(etChatName.checkNull()) return false
+            if(etChatDescription.checkNull()) return false
+            if(etChatMaxPersonnel.checkNull()) return false
+            if(tvChatSelectAddress.checkNull()) return false
+        }
+        return true
+    }
+
+    private fun EditText.checkNull(): Boolean {
+        if (this.text.toString() == "") {
+            this.requestFocus()
+            return true
+        }
+        return false
+    }
+
+    private fun TextView.checkNull(): Boolean {
+        if (this.text.toString() == "") {
+            return true
+        }
+        return false
+    }
+
     override fun onDestroy() {
         _binding = null
         super.onDestroy()
