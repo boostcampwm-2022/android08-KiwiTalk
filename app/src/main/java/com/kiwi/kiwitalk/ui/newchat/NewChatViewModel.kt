abd599a48bcafbd52f87ca8e872b66ff324789bf
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2022-11-29 02:01:46 +0900

    feat: Stream 서버로 데이터 전송

--- app/src/main/java/com/kiwi/kiwitalk/ui/newchat/NewChatViewModel.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/newchat/NewChatViewModel.kt
@@ -1,31 +1,30 @@
 package com.kiwi.kiwitalk.ui.newchat
 
-import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
 import com.kiwi.domain.model.NewChat
-import com.kiwi.domain.model.PlaceList
 import com.kiwi.domain.repository.NewChatRepository
-import com.kiwi.domain.repository.SearchPlaceRepository
+import com.kiwi.kiwitalk.AppPreference
+import com.kiwi.kiwitalk.Const.EMPTY_STRING
+import com.kiwi.kiwitalk.Const.LOGIN_ID_KEY
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.getstream.chat.android.client.utils.toResult
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewChatViewModel @Inject constructor(
-    private val repository: NewChatRepository
+    private val repository: NewChatRepository,
+    private val pref: AppPreference
 ) : ViewModel() {
 
     private val _isChatImage = MutableLiveData<String>()
     val isChatImage: LiveData<String> = _isChatImage
+
+    private val _isChatId = MutableLiveData<String>()
+    val isChatId: LiveData<String> = _isChatId
 
     private val _isNewChatInfo = MutableLiveData<NewChat>()
     val isNewChatInfo: LiveData<NewChat> = _isNewChatInfo
@@ -53,9 +52,14 @@ class NewChatViewModel @Inject constructor(
         _isNewChatInfo.value = newChat
     }
 
-    fun addNewChat(newChat: NewChat) {
+    fun setChatId(){
+        _isChatId.value = pref.getString(LOGIN_ID_KEY, EMPTY_STRING)
+    }
+
+    fun addNewChat(userid: String, currentTime: String, newChat: NewChat) {
         viewModelScope.launch {
-            repository.addFireBaseChat(newChat)
+            repository.addFireBaseChat(userid + currentTime,newChat)
+            repository.addStreamChat(userid,currentTime,newChat)
         }
     }
 }
--- data/src/main/java/com/kiwi/data/datasource/remote/NewChatRemoteDataSourceImpl.kt
+++ data/src/main/java/com/kiwi/data/datasource/remote/NewChatRemoteDataSourceImpl.kt
@@ -4,16 +4,17 @@ import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kiwi.data.model.remote.NewChatRemote
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.User
 import javax.inject.Inject
 
 class NewChatRemoteDataSourceImpl @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val chatClient: ChatClient,
 ): NewChatRemoteDataSource {
-    override suspend fun addFireBaseChat(newChatRemote: NewChatRemote) {
+    override suspend fun addFireBaseChat(cid: String, newChatRemote: NewChatRemote) {
 
         val chat = hashMapOf(
-            "cid" to "djalkfjlkd",
+            "cid" to cid,
             "keyword" to newChatRemote.keywords,
             "lat" to newChatRemote.lat,
             "lng" to newChatRemote.lng
@@ -28,7 +29,35 @@ class NewChatRemoteDataSourceImpl @Inject constructor(
             }
     }
 
-    override suspend fun addStreamChat(newChatRemote: NewChatRemote) {
+    override suspend fun addStreamChat(
+        userId: String,
+        currentTime: String,
+        newChatRemote: NewChatRemote
+    ) {
 
+        val chat = hashMapOf(
+            "image" to newChatRemote.imageUri,
+            "name" to newChatRemote.chatName,
+            "description" to newChatRemote.chatDescription,
+            "address" to newChatRemote.address,
+            "keyword" to newChatRemote.keywords,
+            "max_personnel" to newChatRemote.maxPersonnel
+        )
+
+        val token = chatClient.devToken(userId) // developer 토큰 생성
+        chatClient.connectUser( // 유저 로그인
+            user = User(id = userId),
+            token = token
+        ).enqueue {
+            // Step 4 - 새로운 그룹 (채널) 생성
+            if (it.isSuccess) {
+                chatClient.createChannel(
+                    channelType = "messaging",
+                    channelId = userId + currentTime,
+                    memberIds = listOf(userId),
+                    extraData = chat
+                ).enqueue()
+            }
+        }
     }
 }
