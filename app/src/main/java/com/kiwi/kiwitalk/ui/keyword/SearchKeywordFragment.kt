f482abe1a1fa543b4b2327bcbd32b1755c1106ba
SteelZoo <cjfwn96@gmail.com>
2022-11-30 23:46:51 +0900

    feat: 뒤로가기 버튼 클릭시 키워드 변경 미반영

--- app/src/main/java/com/kiwi/kiwitalk/ui/keyword/SearchKeywordFragment.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/keyword/SearchKeywordFragment.kt
@@ -42,7 +42,7 @@ class SearchKeywordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchKeywordBinding.inflate( inflater,container,false)
-        binding.lifecycleOwner = this
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.viewmodel = searchKeywordViewModel
 
         initToolbar()
@@ -57,6 +57,7 @@ class SearchKeywordFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         searchKeywordViewModel.getAllKeywords()
+        searchKeywordViewModel.saveBeforeKeywords()
     }
 
     private fun setObserve(){
@@ -92,6 +93,7 @@ class SearchKeywordFragment : Fragment() {
             setNavigationOnClickListener {
                 try {
                     val navController = this@SearchKeywordFragment.findNavController()
+                    searchKeywordViewModel.SaveSelectedKeywordOrNot(false)
                     navController.popBackStack()
                 } catch (e: Exception){
                     Log.d("NAV_CONTROLLER", "setNavigationOnClickListener: $e")
@@ -104,6 +106,7 @@ class SearchKeywordFragment : Fragment() {
                     R.id.item_select_keyword -> {
                         try {
                             val navController = this@SearchKeywordFragment.findNavController()
+                            searchKeywordViewModel.SaveSelectedKeywordOrNot(true)
                             navController.popBackStack()
                         } catch (e: Exception) {
                             Log.d("NAV_CONTROLLER", "setOnMenuItemClickListener ERROR: $e")
