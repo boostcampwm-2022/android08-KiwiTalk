d78d155e31bf0c6f3c2572c6aa2d46f7cb7904db
ChoiUS <chldn323@gmail.com>
2022-12-06 00:36:42 +0900

    refactor: 바인딩 null 처리 위치 변경

--- app/src/main/java/com/kiwi/kiwitalk/ui/home/ChatListFragment.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/home/ChatListFragment.kt
@@ -88,8 +88,8 @@ class ChatListFragment : Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
