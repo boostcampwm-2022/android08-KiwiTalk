18423bbb66a0903fbe297401aba8b37bf7187b59
ChoiUS <chldn323@gmail.com>
2022-12-05 17:35:11 +0900

    feat: 읽지 않은 채팅 개수 표시 바인딩 어댑터 구현

--- app/src/main/java/com/kiwi/kiwitalk/ui/BindingAdapter.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/BindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.kiwi.kiwitalk.ui
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.kiwi.kiwitalk.R
@@ -14,4 +16,19 @@ fun setImage(imageView: ImageView, uri: String?) {
         .fallback(R.drawable.ic_baseline_cloud_sync_24)
         .fitCenter()
         .into(imageView)
+}
+
+@BindingAdapter("setUnreadCount")
+fun setUnreadCount(textView: TextView, count: Int?) {
+    when (count) {
+        0, null -> textView.visibility = View.INVISIBLE
+        in 1..999 -> {
+            textView.visibility = View.VISIBLE
+            textView.text = count.toString()
+        }
+        else -> {
+            textView.visibility = View.VISIBLE
+            textView.text = textView.context.getText(R.string.tv_chatList_unreadCountMax)
+        }
+    }
 }
