852d6544b288cf28cd2b6481c45cc7fe969a20c7
KimGyeongyeon <kgy556@naver.com>
2022-12-08 20:16:58 +0900

    refactor: constructor 포맷 변경

--- data/src/main/java/com/kiwi/data/repository/ExitChatRepositoryImpl.kt
+++ data/src/main/java/com/kiwi/data/repository/ExitChatRepositoryImpl.kt
@@ -4,8 +4,9 @@ import com.kiwi.data.datasource.remote.ExitChatRemoteDataSource
 import com.kiwi.domain.repository.ExitChatRepository
 import javax.inject.Inject
 
-class ExitChatRepositoryImpl @Inject constructor(private val datasource: ExitChatRemoteDataSource) :
-    ExitChatRepository {
+class ExitChatRepositoryImpl @Inject constructor(
+    private val datasource: ExitChatRemoteDataSource
+) : ExitChatRepository {
     override suspend fun exitChat(cid: String): Result<String> {
         return datasource.exitChat(cid).map { it.cid }
     }
