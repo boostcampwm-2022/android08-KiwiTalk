257b56881b8f6dd485e527f8c5d384b8ac3dd271
ChoiUS <chldn323@gmail.com>
2022-12-08 22:54:12 +0900

    feat: 프로필 이미지 바꾸는 기능 ViewModel에 추가

--- app/src/main/java/com/kiwi/kiwitalk/ui/home/ProfileViewModel.kt
+++ app/src/main/java/com/kiwi/kiwitalk/ui/home/ProfileViewModel.kt
@@ -1,11 +1,16 @@
 package com.kiwi.kiwitalk.ui.home
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.kiwi.domain.model.Keyword
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.User
 import javax.inject.Inject
 
 
@@ -16,6 +21,8 @@ import javax.inject.Inject
 class ProfileViewModel @Inject constructor(
     val chatClient: ChatClient
 ) : ViewModel() {
+    private val _profileImage = MutableLiveData<String?>()
+    val profileImage: LiveData<String?> = _profileImage
 
     //2way binding
     val myName = MutableLiveData<String>()
@@ -36,6 +43,7 @@ class ProfileViewModel @Inject constructor(
                     Keyword(keywordString as String)
                 }
             }
+            _profileImage.value = user.image.ifBlank { null }
         }
     }
 
@@ -46,16 +54,35 @@ class ProfileViewModel @Inject constructor(
     }
 
     fun setUpdateProfile() {
-        with(chatClient) {
-            getCurrentUser()?.let { user ->
-                myName.value?.let { myNameString ->
-                    user.name = myNameString
+        chatClient.getCurrentUser()?.let { user ->
+            myName.value?.let { myNameString ->
+                user.name = myNameString
+            }
+            myKeywords.value?.let { myKeywordsList ->
+                user.extraData.put("keywords", myKeywordsList.map { it.name })
+            }
+            val uri = profileImage.value
+            if (uri == null) {
+                updateUser(user, uri)
+            } else {
+                val ref = Firebase.storage.reference.child("profile/${user.id}")
+                ref.putFile(Uri.parse(uri)).addOnSuccessListener {
+                    it.storage.downloadUrl.addOnCompleteListener { url ->
+                        updateUser(user, url.result.toString())
+                    }
+                }.addOnFailureListener {
+                    Log.d("NewChatDataSource", "putFile Failure: $it")
                 }
-                myKeywords.value?.let { myKeywordsList ->
-                    user.extraData.put("keywords", myKeywordsList.map { it.name })
-                }
-                updateUser(user).enqueue()
             }
         }
+    }
+
+    private fun updateUser(user: User, uri: String?) {
+        user.image = uri ?: ""
+        chatClient.updateUser(user).enqueue()
+    }
+
+    fun setChatImage(uri: String) {
+        _profileImage.value = uri
     }
 }
